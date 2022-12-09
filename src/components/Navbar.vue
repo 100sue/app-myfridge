442a306930382a9100c663c55c670e0ee2a9e157
appert <scoreur@gmail.com>
2022-12-09 13:36:51 +0100

    fin de l'ux ds categories

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,26 @@
     <header class="bg-white shadow h-16 flex justify-between items-stretch">
       <router-link
         :to="{ name: 'home' }"
-        class="inline-flex items-center h-full px-5 text-3xl text-orange-500 font-bold"
+        class="inline-flex items-center h-full px-5 text-3xl text-orange-500 font-bold font-serif "
       >
-        In My Fridge
+        Like Bocuse
       </router-link>
       <div class="flex items-center gap-1">
         <router-link
           :to="{ name: 'byName' }"
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+          class="inline-flex items-center px-2 h-full font-semibold transition-colors hover:text-orange-600 "
         >
           Search Meals
         </router-link>
         <router-link
           :to="{ name: 'byLetter' }"
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+          class="inline-flex items-center px-2 h-full font-semibold transition-colors hover:text-orange-600"
         >
           Meals By Letter
         </router-link>
         <router-link
           :to="{ name: 'ingredients' }"
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+          class="inline-flex items-center px-2 h-full font-semibold transition-colors hover:text-orange-600"
         >
           Meals By Ingredients
         </router-link>
--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-400">
+  <div class="bg-gray-100">
     <div class="p-8 pb-0">
       <h1 class="text-4xl font-bold mb-4 text-orange-500">Ingredients</h1>
     </div>
